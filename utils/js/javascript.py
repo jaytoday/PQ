3f686c686adb8e468b21bc2d3b2dd944e2d31d7d
james <james@james-desktop.(none)>
2008-11-25 17:10:35 -0800

    auto-minifying JS

--- utils/js/javascript.py
+++ utils/js/javascript.py
@@ -1,8 +1,18 @@
 
 
 from StringIO import StringIO
+import os
 
-
+def debug_mode():
+	if os.environ.get('SERVER_SOFTWARE','').startswith('Devel'):
+	  HOST='local'
+	  debug = True
+	elif os.environ.get('SERVER_SOFTWARE','').startswith('Goog'):
+	  HOST='google'
+	  debug = False
+	return debug
+  
+  
 def jsmin(js):
     ins = StringIO(js)
     outs = StringIO()
@@ -196,11 +206,13 @@ import wsgiref.handlers
 
 jsm = JavascriptMinify()
 
-memCacheExpire = 900
+memCacheExpire = 100000
+
+debugExpire = 10
+
 
 def getFileName(path):
-  regex = re.compile('/[a-zA-Z_]+/([a-zA-Z0-9_\-/]+\.(js|css))$')
-  return os.path.join(os.path.dirname(__file__) + '/.../static/' + regex.search(path).groups()[0])
+  return os.path.join(os.path.dirname(__file__) + '/../..' + path)
 
 def getFileContent(filename):  
   ospath = os.path.join(os.path.dirname(__file__), filename)
@@ -211,21 +223,23 @@ def minify(filename):
   path = os.path.join(os.path.dirname(__file__), filename)        
   input = open(path, 'r')
   output = StringIO.StringIO()
-
   jsm.minify(input, output)
-
   return output.getvalue()  
 
-class JsMinify(webapp.RequestHandler):
+class Minify(webapp.RequestHandler):
   def get(self):
-        
     data = memcache.get(self.request.path)    
-
     if data is None:
       filename = getFileName(self.request.path)
-      data = minify(filename)   
-      memcache.add(self.request.path, data, memCacheExpire)      
-
+      #dm = debug_mode()
+      dm = False
+      if dm: # debug mode - don't minify
+        data = getFileContent(filename)
+        expire = debugExpire
+      else: 
+        data = minify(filename)
+        expire = memCacheExpire
+      memcache.add(self.request.path, data, expire)      
     self.response.headers['Content-Type'] = 'text/javascript'
     self.response.out.write(data)
 
@@ -242,7 +256,7 @@ class JsMinify_(webapp.RequestHandler):
 class JsNormal(webapp.RequestHandler):
   def get(self):      
 
-    data = memcache.get(self.request.path)    
+    data = memcache.get(self.request.path)
 
     if data is None:
       filename = getFileName(self.request.path)
@@ -260,20 +274,24 @@ class JsNormal_(webapp.RequestHandler):
     self.response.headers['Content-Type'] = 'text/javascript'
     self.response.out.write(data)
 
+
 apps_binding = []
 
-# serving normal javascript file from mem cache
-apps_binding.append(('/static/scripts/.*', JsNormal))
+apps_binding.append(('/static/.*', Minify))
 
 # serving normal javascript file directly from disk
 apps_binding.append(('/static/scripts_/.*', JsNormal_))
 
-# serving minified javascript file from mem cache
-apps_binding.append(('/static/scripts_min/.*', JsMinify))
+# serving normal javascript file from mem cache
+apps_binding.append(('/static/scripts/.*', JsNormal))
 
 # serving minified javascript file directly from disk
 apps_binding.append(('/static/scripts/.*', JsMinify_))
 
 
+    
 application = webapp.WSGIApplication(apps_binding, debug=True)
 wsgiref.handlers.CGIHandler().run(application)
+
+
+
