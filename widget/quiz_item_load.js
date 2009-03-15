a3ba03f71b785f21ed863eb6cea03146d1ecec84
Ben <ben@coderholic.com>
2009-03-15 17:44:57 +0000

    Quiz widget resizing is now animated. Only tested in FF3 so far. Time taken: 1.5h

--- widget/quiz_item_load.js
+++ widget/quiz_item_load.js
@@ -1,212 +1,217 @@
-
 function quizItemLoad(quizItem, html, s)
- {
-                 	
-
-// set the quiz content -- it can be seen
-$.plopquiz.quiz_content.html(html);
-
-
-// hide the answers for now
-$('.answer', $.plopquiz.answer_container).hide();
-
-// this occasionally results in things fading in twice...the solution is to fade in the element after a pause.
-$.plopquiz.quiz_loader.attr('class', quizItem.item_type + '_load').animate({opacity: 0}, {  duration: 0, complete: function()
-{ $.plopquiz.quiz_inner_content.animate({opacity:1},{duration:200}).removeClass('disabled'); } 
-}).hide();
-
-
-/* 
-TODO: Quiz Frame Animation
-
-Frame is re-positioned and given new dimensions. jQuery.animate() should be used to ease these.
-
-Since the height and width is defined by classes in stylesheets, this may take a little bit of refactoring...
-
-*/ 
-
-// apply new class for quiz content for item_type - specified in pqwidget.css
-$.plopquiz.quiz_content.attr('class', quizItem.item_type + '_content'); 
-$.plopquiz.answer_container.attr('class', quizItem.item_type + '_answers');
-$('#quiz_inner', $.pq_wrapper ).attr('class', quizItem.item_type + '_quiz_inner');
-$('.quiz_selection', $.plopquiz.quiz_content).attr('id', quizItem.item_type + '_quiz_selection');
-$.plopquiz.timer_wrapper.attr('id', quizItem.item_type);
-
-// set new quiz frame position
-var margin_left = ( parseInt($.plopquiz.quiz_content.css('width')) / 2 ) + 15; 
-var margin_top = ( parseInt($.plopquiz.quiz_content.css('height')) / 2 ) + 15;
-$.plopquiz.quiz_outer.css({'margin-left': -margin_left, 'margin-top': -margin_top }); 
-
-
-/*
- *
- *  Initialize Answers
- *
- */
-
-
-for ( var i in quizItem.answers )
 {
+	// Remove the existing content
+	$.plopquiz.quiz_content.html('');
+	
+	// hide the answers for now
+	$('.answer', $.plopquiz.answer_container).hide();
 
-		if (quizItem.item_type == 'intro' || quizItem.item_type == 'begin_quiz' || quizItem.item_type == 'quiz_complete'){  
-		var this_answer = $('#confirm', $.plopquiz.answer_container);
-		}else{ 
-		var this_answer = $('.answer:eq(' + i + ')', $.plopquiz.answer_container); 
+	// this occasionally results in things fading in twice...the solution is to fade in the element after a pause.
+	$.plopquiz.quiz_loader.attr('class', quizItem.item_type + '_load').animate({opacity: 0}, {  duration: 0, complete: function()
+		{ $.plopquiz.quiz_inner_content.animate({opacity:1},{duration:200}).removeClass('disabled'); } 
+	}).hide();
+
+	// apply new class for quiz content for item_type - specified in pqwidget.css. Called after the resize
+	function setNewStyles() {
+		// Display the new content
+		$.plopquiz.quiz_content.html(html);
+		
+		$.plopquiz.quiz_content.attr('class', quizItem.item_type + '_content'); 
+		$.plopquiz.answer_container.attr('class', quizItem.item_type + '_answers');
+		$('#quiz_inner', $.pq_wrapper ).attr('class', quizItem.item_type + '_quiz_inner');
+		$('.quiz_selection', $.plopquiz.quiz_content).attr('id', quizItem.item_type + '_quiz_selection');
+		$.plopquiz.timer_wrapper.attr('id', quizItem.item_type);
+		
+		afterResize();
+	}
+
+	currentWidth = $.plopquiz.quiz_content.css('width');
+	currentHeight = $.plopquiz.quiz_content.css('height');
+
+	if(currentHeight == 'auto' || currentWidth == 'auto') {
+		setNewStyles();
+
+		// set new quiz frame position
+		var margin_left = ( parseInt($.plopquiz.quiz_content.css('width')) / 2 ) + 15; 
+		var margin_top = ( parseInt($.plopquiz.quiz_content.css('height')) / 2 ) + 15;
+		$.plopquiz.quiz_outer.css({'margin-left': -margin_left, 'margin-top': -margin_top }); 
+	}
+	else {
+		// Add a hidden element with the new style so we can get the new dimensions
+		newStyle = quizItem.item_type + '_content';
+		tempElem = jQuery("<div style='display:none' class='" + newStyle + "'/>");
+		$.plopquiz.quiz_content.append(tempElem);
+		newWidth = tempElem.css('width');
+		newHeight = tempElem.css('height');
+		tempElem.remove();
+		
+		var animDuration = 'normal';
+		
+		// Resize animation
+		var margin_left = ( parseInt(newWidth) / 2 ) + 15; 
+		var margin_top = ( parseInt(newHeight) / 2 ) + 15;
+		$.plopquiz.quiz_outer.animate({'marginLeft': -margin_left, 'marginTop': -margin_top }, {duration: animDuration, queue:false});  
+		$.plopquiz.quiz_content.animate({width: newWidth, height: newHeight}, {duration: animDuration, queue:false, complete: setNewStyles});
+		
+	}
+	
+
+	// Do everything else once we've completed the resize
+	function afterResize() {
+		for ( var i in quizItem.answers )
+		{
+				if (quizItem.item_type == 'intro' || quizItem.item_type == 'begin_quiz' || quizItem.item_type == 'quiz_complete'){  
+				var this_answer = $('#confirm', $.plopquiz.answer_container);
+				}else{ 
+				var this_answer = $('.answer:eq(' + i + ')', $.plopquiz.answer_container); 
+				}
+
+				this_answer
+				.show()
+				.data("disabled", false)
+				.find(".answertext")
+				.html(quizItem.answers[i]);
+		} 
+
+		// not all items are timed (instructions)
+		if(quizItem.timed)
+		$.plopquiz.timer_wrapper.show();
+		else
+		$.plopquiz.timer_wrapper.hide();
+
+		// not all items need skipping
+		if(!quizItem.noSkip)
+		$("#skip", $.plopquiz.answer_container).show();
+		else
+		$("#skip", $.plopquiz.answer_container).hide();
+
+
+		$.plopquiz.answers.removeClass('disabled').data("disabled", false);
+
+
+		/*
+		 * 
+		 * 
+		* Setup special cases for quiz frames
+		* 
+		* 
+		*/
+
+
+
+		if(quizItem.item_type == "intro")
+		{
+
+
+		{% include "quiz_intro.js" %}
+
+
 		}
 
-		this_answer
-		.show()
-		.data("disabled", false)
-		.find(".answertext")
-		.html(quizItem.answers[i]);
-		
-
-
-} 
-
-// not all items are timed (instructions)
-if(quizItem.timed)
-$.plopquiz.timer_wrapper.show();
-else
-$.plopquiz.timer_wrapper.hide();
-
-// not all items need skipping
-if(!quizItem.noSkip)
-$("#skip", $.plopquiz.answer_container).show();
-else
-$("#skip", $.plopquiz.answer_container).hide();
-
-
-$.plopquiz.answers.removeClass('disabled').data("disabled", false);
-
-
-/*
- * 
- * 
-* Setup special cases for quiz frames
-* 
-* 
-*/
-
-
-
-if(quizItem.item_type == "intro")
-{
-
-
-{% include "quiz_intro.js" %}
-
-
-}
 
 
 
 
 
-
-if(quizItem.item_type == "instructions")
-{
+		if(quizItem.item_type == "instructions")
+		{
 	
-//special handler for instructions 1 hovering
-var i1mouseOverCount = 0;
-var i1mouseOver = function()
-{
-// unbind is to prevent incrementing on the same button
-$(this).unbind('mouseover',i1mouseOver);
-// both buttons have been hovered? good done
-if(++i1mouseOverCount >= 2)
-{
-$('#example_1,#example_2').toggle();
-$.plopquiz.settings.instructions.i1complete = true;
-i1mouseOverCount = null;
-}
-};
+		//special handler for instructions 1 hovering
+		var i1mouseOverCount = 0;
+		var i1mouseOver = function()
+		{
+		// unbind is to prevent incrementing on the same button
+		$(this).unbind('mouseover',i1mouseOver);
+		// both buttons have been hovered? good done
+		if(++i1mouseOverCount >= 2)
+		{
+		$('#example_1,#example_2').toggle();
+		$.plopquiz.settings.instructions.i1complete = true;
+		i1mouseOverCount = null;
+		}
+		};
 
 
-$("#skip_tutorial", $.plopquiz.quiz_content).click(function()
-{
-$.plopquiz.currentItem = 3;
-$.plopquiz.loadItem();
-});
+		$("#skip_tutorial", $.plopquiz.quiz_content).click(function()
+		{
+		$.plopquiz.currentItem = 3;
+		$.plopquiz.loadItem();
+		});
 
 
-$(".answer", $.plopquiz.answer_container).mouseover(i1mouseOver);
-}
-
-
-
+		$(".answer", $.plopquiz.answer_container).mouseover(i1mouseOver);
+		}
 
 
 
-if(quizItem.item_type == "instructions2")
-{
+
+
+
+		if(quizItem.item_type == "instructions2")
+		{
 	
-$('#skip', $.plopquiz.answer_container).hide();
+		$('#skip', $.plopquiz.answer_container).hide();
 
-$("#skip_tutorial", $.plopquiz.quiz_content).click(function()
-{
-$.plopquiz.currentItem = 3;
-$.plopquiz.loadItem();
-});
-}
-
-
-
-
-
-if(quizItem.item_type == "begin_quiz")
-{
-
-$('#confirm', $.plopquiz.answer_container)
-  .removeClass('intro_quiz').addClass('begin_quiz').find('span.continue_button').removeClass('intro_button').addClass('begin_button').text('Start the Quiz').end()
-  .animate({ opacity:0},{duration: 0, 
-                        complete: function(){  var $button = $('#confirm', $.plopquiz.answer_container); $button.data('disabled', true);  $("div#begin_quiz_quiz_selection", $.plopquiz.quiz_content).animate({height:170},{ duration: 1000, 
-                        complete: function(){  $("#click_below", $.plopquiz.quiz_content).animate({opacity:1},{ duration: 1000,     
-                        complete: function(){  $button.animate({opacity:1},{duration: 1000 });  $button.data('disabled', false);  
-                         }});   
-                         }}); 
-						 }});        
-
-                
-$.plopquiz.settings.instructions.completed = true;
-
-}
+		$("#skip_tutorial", $.plopquiz.quiz_content).click(function()
+		{
+		$.plopquiz.currentItem = 3;
+		$.plopquiz.loadItem();
+		});
+		}
 
 
 
 
 
+		if(quizItem.item_type == "begin_quiz")
+		{
 
-if(quizItem.item_type == "quiz_item")
-{
-// reset the blank space
-$('#blank', $.plopquiz.quiz_content).empty();
-// hide the question until everything is loaded
-$.plopquiz.quiz_content.css('opacity', 0);
-$.plopquiz.answer_text.animate({opacity: 0}, 0);
-$.plopquiz.answer_load_icons.animate({opacity: 1}, 300);
-$.plopquiz.answers.data('disabled', true);
-}
+		$('#confirm', $.plopquiz.answer_container)
+		  .removeClass('intro_quiz').addClass('begin_quiz').find('span.continue_button').removeClass('intro_button').addClass('begin_button').text('Start the Quiz').end()
+		  .animate({ opacity:0},{duration: 0, 
+				                complete: function(){  var $button = $('#confirm', $.plopquiz.answer_container); $button.data('disabled', true);  $("div#begin_quiz_quiz_selection", $.plopquiz.quiz_content).animate({height:170},{ duration: 1000, 
+				                complete: function(){  $("#click_below", $.plopquiz.quiz_content).animate({opacity:1},{ duration: 1000,     
+				                complete: function(){  $button.animate({opacity:1},{duration: 1000 });  $button.data('disabled', false);  
+				                 }});   
+				                 }}); 
+								 }});        
 
+				        
+		$.plopquiz.settings.instructions.completed = true;
 
-
-if(quizItem.item_type == "quiz_complete")
-{
-//update text for Button
-$('#quiz_answers #confirm').removeClass('begin_quiz').addClass('quiz_complete').find('span.continue_button').text('See Results').show();
-
- }                
-
-
-// short delay to ensure everything is loaded
-setTimeout(function()
-{
-$.event.trigger('quizItemLoaded', [ quizItem ]);
-},100);
+		}
 
 
 
+
+
+
+		if(quizItem.item_type == "quiz_item")
+		{
+		// reset the blank space
+		$('#blank', $.plopquiz.quiz_content).empty();
+		// hide the question until everything is loaded
+		$.plopquiz.quiz_content.css('opacity', 0);
+		$.plopquiz.answer_text.animate({opacity: 0}, 0);
+		$.plopquiz.answer_load_icons.animate({opacity: 1}, 300);
+		$.plopquiz.answers.data('disabled', true);
+		}
+
+
+
+		if(quizItem.item_type == "quiz_complete")
+		{
+		//update text for Button
+		$('#quiz_answers #confirm').removeClass('begin_quiz').addClass('quiz_complete').find('span.continue_button').text('See Results').show();
+
+		 }                
+
+
+		// short delay to ensure everything is loaded
+		setTimeout(function()
+		{
+		$.event.trigger('quizItemLoaded', [ quizItem ]);
+		},100);
+	}
                                 
-                        } //end of quizitemLoad
-                      
-   
+} //end of quizitemLoad
